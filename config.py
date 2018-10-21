6f92cf402900c455bdcf482939f9d8b61b752111
Michael Fedell <michaelfedell14@gmail.com>
2018-10-20 19:44:50 -0500

    add user and posts table for blog

--- config.py
+++ config.py
@@ -6,7 +6,11 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or \
+        'boil-em-mash-em-stick-em-in-a-stew'  # default
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')  # default
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
